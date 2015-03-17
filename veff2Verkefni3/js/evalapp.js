7bf4f53e2d404a084da97253dc524ec1891078af
hogniR <hogni@ingimarsson.com>
2015-03-17 19:15:50 +0000

    commented out routes that are not complete for error handling

--- veff2Verkefni3/js/evalapp.js
+++ veff2Verkefni3/js/evalapp.js
@@ -4,7 +4,7 @@ EvalClient.config (
 	function ($routeProvider) {
 	$routeProvider
 		.when('/login', { templateUrl: 'Views/login.html', controller: 'LoginController'})
-		.when('/evals/:user/', {templateUrl: 'Views/evals.html', controller 'EvalsController'})
+		//.when('/evals/:user/', {templateUrl: 'Views/evals.html', controller 'EvalsController'})
 		//.when('/evaluation/:user/:evaluation/', {templateUrl: 'Views/evaluation.html', controller 'EvaluationController'})
 		//.when('/evaltemplate/:user/', {templateUrl: 'Views/evaltemplate.html', controller 'EvalTemplateController'})
 		.otherwise({
