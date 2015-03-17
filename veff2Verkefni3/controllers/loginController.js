c704d6a1a21ebc39e4f4b173eebfc5ddda47a90c
TheSolvemaster <solvihj@gmail.com>
2015-03-17 14:29:31 +0000

    fixing merges

--- veff2Verkefni3/controllers/loginController.js
+++
@@ -1,9 +0,0 @@
-var EvalClient = angular.module('ChatClient');
-
-EvalClient.controller('LoginController', function ($scope, socket, $location, $rootScope, $routeParams) {
-	$scope.errorMessage = '';
-
-	$scope.login = function() {
-		
-	}
-})
